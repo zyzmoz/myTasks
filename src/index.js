0f8f638702c38002eed7e1befd3b47206dfd5f13
Daniel Cunha <danielbrandaodacunha@gmail.com>
2018-01-16 15:28:30 -0200

    Foundation added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './pages/Home';
+import Foundation from 'react-foundation';
+import './assets/css/foundation.min.css';
 
 ReactDOM.render(
   <HomePage />,
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import Foundation,{ Callout, Colors, Button } from 'react-foundation';
 
 
 class HomePage extends Component {
+  constructor(){
+    super();
+    this.state = {
+      click: 0
+    }
+  }
   render(){
+    let msg = "";
+    if (this.state.click > 0)
+      msg = "Click count: " + this.state.click;      
+
     return(
-      <div>
-        <h2>Hello World</h2>
+      <div className="grid-container">
+        <Callout color={Colors.PRIMARY}>
+          <h2>Hello World</h2>
+        </Callout>
+        <p>{msg}</p>
+        <Button onClick={() => this.setState({click: this.state.click + 1})} color={Colors.SECONDARY}>Here</Button>
       </div>
     );
   }
