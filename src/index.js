916b0e99bf1f15bc13b4f9d0e0b831f8e2015fd7
Daniel Cunha <danielbrandaodacunha@gmail.com>
2018-01-16 11:10:13 -0200

    Parcel Config

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomePage from './pages/Home';
+
+ReactDOM.render(
+  <HomePage />,
+  document.getElementById('root')
+);
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class HomePage extends Component {
+  render(){
+    return(
+      <div>
+        <h2>Hello World</h2>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
